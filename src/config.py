583209820e84ca7eac298c47206a79a735a7c9aa
SimonStnn <simon@cerm.be>
2025-08-18 15:59:51 +0000

    Add rich logging

--- src/config.py
+++ src/config.py
@@ -1,20 +1,47 @@
+import logging
 import os
 
 from dotenv import load_dotenv
 from pydantic import Field
 from pydantic_settings import BaseSettings
+from rich.logging import RichHandler
 
 ENV_PATH: str = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", ".env"))
-
 load_dotenv(ENV_PATH)
-print("Loaded env from: ", ENV_PATH)
+
+
+def setup_logging():
+    handler = RichHandler(
+        rich_tracebacks=True,
+        markup=True,
+        show_time=True,
+        show_level=True,
+        show_path=True,
+        log_time_format=settings.log.datefmt,
+        omit_repeated_times=False,
+    )
+    root = logging.getLogger()
+    level = getattr(logging, str(settings.log.level), None)
+    if not isinstance(level, int):
+        level = logging.INFO
+    root.setLevel(level)
+    root.handlers = [handler]
 
 
 class JIRAConfig(BaseSettings):
     server: str = Field(frozen=True)
     email: str = Field(frozen=True, min_length=1)
-    api_token: str = Field(frozen=True, min_length=1)
+    api_token: str = Field(frozen=True, min_length=1, exclude=True)
     user_agent: str = Field(default="Cerm7-AI-project", frozen=True)
+
+
+class LoggerConfig(BaseSettings):
+    level: str = Field(default="INFO", frozen=True)
+    format: str = Field(
+        default="%(asctime)s [%(name)s] %(levelname)s %(message)s", frozen=True
+    )
+    datefmt: str = Field(default="%Y-%m-%d %H:%M:%S", frozen=True)
+    name: str = Field(frozen=True)
 
 
 class Settings(BaseSettings):
@@ -25,10 +52,36 @@ class Settings(BaseSettings):
         user_agent=os.getenv("JIRA_USER_AGENT"),  # type: ignore
     )
 
+    log: LoggerConfig = LoggerConfig(
+        level=os.getenv("LOG_LEVEL", "INFO"),
+        format=os.getenv(
+            "LOG_FORMAT", "%(asctime)s [%(name)s] %(levelname)s %(message)s"
+        ),
+        datefmt=os.getenv("LOG_DATEFMT", "%Y-%m-%d %H:%M:%S"),
+        name=os.getenv("LOG_NAME", "Cerm7-AI-project"),
+    )
+
     class Config:
         env_file = ".env"
 
 
 settings = Settings()
 
-print(settings)
+setup_logging()
+log = logging.getLogger(settings.log.name)
+
+log.info("Loaded env from: %s", ENV_PATH)
+
+
+if __name__ == "__main__":
+    from rich import print
+
+    print(settings)
+
+    log.setLevel("DEBUG")
+
+    log.debug("Debug message")
+    log.info("Info message")
+    log.warning("Warning message")
+    log.error("Error message")
+    log.critical("Critical message")
--- src/main.py
+++ src/main.py
@@ -1,68 +1,35 @@
 from __future__ import annotations
 
-import re
-
 from jira import JIRA
 from requests_toolbelt import user_agent
 
-from config import settings
+from config import log, settings
 
 __version__ = "0.0.0"
 
 assert __version__ is not None, "Version must be set"
 
 
-# By default, the client will connect to a Jira instance started from the Atlassian Plugin SDK
-jira = JIRA(
-    settings.jira.server,
-    basic_auth=(settings.jira.email, settings.jira.api_token),
-    options={
-        "headers": {"User-Agent": user_agent(settings.jira.user_agent, __version__)}
-    },
-)
+def main():
+    log.info("Application started.")
 
-# Get all projects viewable by anonymous users.
-projects = jira.projects()
+    # By default, the client will connect to a Jira instance started from the Atlassian Plugin SDK
+    jira = JIRA(
+        settings.jira.server,
+        basic_auth=(settings.jira.email, settings.jira.api_token),
+        options={
+            "headers": {"User-Agent": user_agent(settings.jira.user_agent, __version__)}
+        },
+    )
 
-# Sort available project keys, then return the second, third, and fourth keys.
-keys = sorted(project.key for project in projects)[2:5]
+    # Get all projects viewable by anonymous users.
+    projects = jira.projects()
 
-print(projects)
+    # Sort available project keys, then return the second, third, and fourth keys.
+    keys = sorted(project.key for project in projects)[2:5]
 
-# exit()
+    log.info(f"Projects: {keys}")
 
-# Get an issue.
-issue = jira.issue("JRA-1330")
-# Find all comments made by Atlassians on this issue.
-atl_comments = [
-    comment
-    for comment in issue.fields.comment.comments
-    if re.search(r"@atlassian.com$", comment.author.key)
-]
 
-# Add a comment to the issue.
-jira.add_comment(issue, "Comment text")
-
-# Change the issue's summary and description.
-issue.update(
-    summary="I'm different!", description="Changed the summary to be different."
-)
-
-# Change the issue without sending updates
-issue.update(notify=False, description="Quiet summary update.")
-
-# You can update the entire labels field like this
-issue.update(fields={"labels": ["AAA", "BBB"]})
-
-# Or modify the List of existing labels. The new label is unicode with no
-# spaces
-issue.fields.labels.append("new_text")
-issue.update(fields={"labels": issue.fields.labels})
-
-# Send the issue away for good.
-issue.delete()
-
-# Linking a remote jira issue (needs applinks to be configured to work)
-issue = jira.issue("JRA-1330")
-issue2 = jira.issue("XX-23")  # could also be another instance
-jira.add_remote_link(issue.id, issue2)
+if __name__ == "__main__":
+    main()
